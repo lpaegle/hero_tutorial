72a3f965aec826df8103a5fe3055598bde9c8eef
lpaegle <livijapaegle@gmail.com>
2021-07-14 20:33:02 +0300

    Super hero grade with switch

--- src/HeroApplication.java
+++ src/HeroApplication.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HeroApplication {
 
     public static void main(String[] args) {
@@ -32,6 +34,7 @@ public class HeroApplication {
         System.out.println();
         System.out.println("Hero name: " + heroName);
         System.out.println("Hero city: " + heroCity);
+        System.out.println("----------------");
         System.out.println("Hero's coolest power: " + heroPower2);
         System.out.println("Hero's another power: " + heroPower1 + ". $$$");
         System.out.println("It's " + isEvil + " that hero is evil.");
@@ -40,6 +43,84 @@ public class HeroApplication {
         System.out.println("Universe hero lives: " + universe);
         System.out.println("*****************");
 
-    }
+        //How many cookies can superhero purchase for their salary?
+        //1 cookie costs = 1.29 EUR
+//        System.out.println();
+//        System.out.println("---- Financials ----");
+//        // .floor - round down the number
+//        int numOfCookies = (int) Math.floor(heroSalary / 1.29);
+//        System.out.println("Hero can purchase " + numOfCookies + " cookies.");
+//        double averageDailySalary = Math.round(heroSalary / 30 * 100) / 100;
+//        System.out.println("Hero's average daily salary is " + averageDailySalary);
 
+        if (isEvil) {
+            System.out.println("Hero is a villain. ");
+        } else {
+            System.out.println("Hero is the protector of the city.");
+        }
+
+        switch (heroCity) {
+            case "Ney York":
+                heroSalary = 2000;
+                break;
+            case "Riga":
+                heroSalary = 1200.91;
+                break;
+            case "Metropolis":
+                heroSalary = 1000000;
+                break;
+            default:
+                heroSalary = 1000;
+        }
+
+        System.out.println("Hero's new salary is " + heroSalary);
+        System.out.println();
+        System.out.println("---- Financials ----");
+        // .floor - round down the number
+        int numOfCookies = (int) Math.floor(heroSalary / 1.29);
+        System.out.println("Hero can purchase " + numOfCookies + " cookies.");
+        double averageDailySalary = Math.round(heroSalary / 30 * 100) / 100;
+        System.out.println("Hero's average daily salary is " + averageDailySalary);
+
+        System.out.println("------------------");
+
+        //Create a char type variable which represents the grade level of the deed performed by hero,
+        // for example ‘A’, ‘B’, ‘C’,..., ‘H’.
+        //Using switch control, please print out how good the student was in the exam.
+
+        System.out.println("Please enter Super Hero's grade: ");
+
+        //Scanner for char
+        Scanner scanner = new Scanner(System.in);
+        char grade = scanner.next().charAt(0);
+
+        switch(grade){
+            case 'A':
+//          For two cases with same output, you can write like this
+//                System.out.println("Perfect! You are so brave!");
+//                break;
+            case 'B':
+                System.out.println("Perfect! You are so brave!");
+                break;
+            case 'C':
+                System.out.println("Good! But You can do better!");
+                break;
+            case 'D':
+                System.out.println("It is not good! You should choose your bad or good side!");
+                break;
+            case 'E':
+                System.out.println("It is not good! You should choose your bad or good side!");
+                break;
+            case 'G':
+                System.out.println("Bad, you are true villain");
+                break;
+            default:
+                System.out.println("No such grade. Please check again!");
+
+        }
+
+
+    }
 }
+
+
